b0b1aa8f01f4f245fd5bb71c788f7da9ab973e91
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-08 17:46:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import DynamicField from './DynamicField'
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <DynamicField/>
     </div>
   )
 }
